85bfd15f22e70c4510c98765d496b38d7e388524
k-equals0 <sallykey@naver.com>
2026-02-05 00:04:54 +0900

    리팩토링(2) 기능 분리

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,13 +3,14 @@ package org.example;
 import java.util.Scanner;
 
 public class App {
+
+    Scanner sc = new Scanner(System.in);    //인스턴수 변수로 꺼내기 -> 함수들이 공동으로 사용
+    int lastId = 0;
+
+    Quote[] quotes = new Quote[10];
+    int lastQuoteIndex = -1;
+
     public void run(){
-
-        Scanner sc = new Scanner(System.in);
-        int lastId = 0;
-
-        Quote[] quotes = new Quote[10];
-        int lastQuoteIndex = -1;
 
         System.out.println("== 명언 앱 ==");
 
@@ -20,28 +21,36 @@ public class App {
             if(cmd.equals("종료"))
                 break;
 
-            if(cmd.equals("등록")){
-                System.out.print("명언 : ");
-                String content = sc.nextLine();
-                System.out.print("작가 : ");
-                String author = sc.nextLine();
-
-                Quote quote = new Quote();
-
-                quote.id = ++lastId;
-                quote.content = content;
-                quote.author = author;
-
-                quotes[++lastQuoteIndex] = quote;
-                System.out.println(lastId + "번 명언이 등록되었습니다.");
+            if(cmd.equals("등록")){       //등록 기능 분리
+                actionWrite();            //command + 함수명 누르면 해당 함수로 이동
             }
-            else if(cmd.equals("목록")){
-                System.out.println("번호 / 작가 / 명언\n--------------------------");
-                for(int i=lastQuoteIndex; i>=0; i--){
-                    Quote foundedQuote = quotes[i];
-                            System.out.println(foundedQuote.id + " / " + foundedQuote.author + " / " + foundedQuote.content);
-                }
+            else if(cmd.equals("목록")){  //목록 기능 분리
+                actionList();
             }
+        }
+    }
+
+    private void actionWrite() {
+        System.out.print("명언 : ");
+        String content = sc.nextLine();
+        System.out.print("작가 : ");
+        String author = sc.nextLine();
+
+        Quote quote = new Quote();
+
+        quote.id = ++lastId;
+        quote.content = content;
+        quote.author = author;
+
+        quotes[++lastQuoteIndex] = quote;
+        System.out.println(lastId + "번 명언이 등록되었습니다.");
+    }
+
+    private void actionList() {
+        System.out.println("번호 / 작가 / 명언\n--------------------------");
+        for (int i = lastQuoteIndex; i >= 0; i--) {
+            Quote foundedQuote = quotes[i];
+            System.out.println(foundedQuote.id + " / " + foundedQuote.author + " / " + foundedQuote.content);
         }
     }
 }
