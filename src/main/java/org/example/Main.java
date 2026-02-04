372e6830f98c751e6ef7e8555ede9b049cf44840
k-equals0 <sallykey@naver.com>
2026-02-04 22:18:14 +0900

    목록 기능 구현(2)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,9 @@ public class Main {
     static void main() {
 
         Scanner sc = new Scanner(System.in);
-        int id = 1;
+        int id = 0;
+        String quote = "";  //지역변수로 선언
+        String author = "";
 
         System.out.println("== 명언 앱 ==");
 
@@ -19,17 +21,17 @@ public class Main {
 
             if(cmd.equals("등록")){
                 System.out.print("명언 : ");
-                String quote = sc.nextLine();
+                quote = sc.nextLine();
                 System.out.print("작가 : ");
-                String author = sc.nextLine();
+                author = sc.nextLine();
 
-                System.out.println(id + "번 명언이 등록되었습니다.");
                 id++;
+                System.out.println(id + "번 명언이 등록되었습니다.");
             }
             else if(cmd.equals("목록")){
                 System.out.println("번호 / 작가 / 명언\n--------------------------");
-                for(int i=id-1; i>0; i--){
-                    System.out.println(id + " / " + author + " / " + quote);    //명언, 작가 저장 필요
+                for(int i=id; i>0; i--){
+                    System.out.println(id + " / " + author + " / " + quote);
                 }
             }
         }
