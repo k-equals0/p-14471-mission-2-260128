8a8e321748166ef48dee32d8f795d82e24a59f9c
k-equals0 <sallykey@naver.com>
2026-02-05 00:39:15 +0900

    리팩토링(3) 상호작용부, 데이터처리부로 분리

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,32 +10,37 @@ public class App {
     Quote[] quotes = new Quote[10];
     int lastQuoteIndex = -1;
 
-    public void run(){
+    public void run() {
 
         System.out.println("== 명언 앱 ==");
 
-        while(true){
+        while (true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine();
 
-            if(cmd.equals("종료"))
+            if (cmd.equals("종료"))
                 break;
 
-            if(cmd.equals("등록")){       //등록 기능 분리
-                actionWrite();            //command + 함수명 누르면 해당 함수로 이동
-            }
-            else if(cmd.equals("목록")){  //목록 기능 분리
+            if (cmd.equals("등록")) {
+                actionWrite();
+            } else if (cmd.equals("목록")) {
                 actionList();
             }
         }
     }
 
+    //App 내부에서만 동작하는 함수들이기 때문에 private으로 설정
     private void actionWrite() {
         System.out.print("명언 : ");
         String content = sc.nextLine();
         System.out.print("작가 : ");
         String author = sc.nextLine();
 
+        write(content, author);     //write 기능 분리
+        System.out.println(lastId + "번 명언이 등록되었습니다.");
+    }
+
+    private void write(String content, String author) {
         Quote quote = new Quote();
 
         quote.id = ++lastId;
@@ -43,14 +48,25 @@ public class App {
         quote.author = author;
 
         quotes[++lastQuoteIndex] = quote;
-        System.out.println(lastId + "번 명언이 등록되었습니다.");
     }
 
     private void actionList() {
         System.out.println("번호 / 작가 / 명언\n--------------------------");
-        for (int i = lastQuoteIndex; i >= 0; i--) {
-            Quote foundedQuote = quotes[i];
-            System.out.println(foundedQuote.id + " / " + foundedQuote.author + " / " + foundedQuote.content);
+        Quote[] foundedQuotes = findList(); //list 기능 분리, 최신순으로 정리된 명언 목록 받아옴
+
+        for (Quote quote : foundedQuotes) {
+            System.out.printf("%d / %s / %s\n", quote.id, quote.author, quote.content); //printf
         }
+    }
+
+    private Quote[] findList() {
+        Quote[] foundedQuotes = new Quote[lastQuoteIndex + 1];
+        int foundedQuoteIndex = -1;
+
+        for (int i = lastQuoteIndex; i >= 0; i--) {
+            Quote foundedQuote = quotes[i];                     //저장소에서 최신명언부터 꺼냄
+            foundedQuotes[++foundedQuoteIndex] = foundedQuote;  //0번 인덱스부터 최신 명언 채움
+        }
+        return foundedQuotes;   //출력만 하면 되는 상태로 돌려줌
     }
 }
