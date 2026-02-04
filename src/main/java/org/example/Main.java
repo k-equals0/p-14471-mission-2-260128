85c457a18a2eb2d9b51583ea76981b4fd6e80552
k-equals0 <sallykey@naver.com>
2026-02-04 21:55:03 +0900

    등록, 종료 기능 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,9 +8,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
 
-        String cmd = sc.nextLine();
+        while(true){
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
 
+            if(cmd.equals("종료"))
+                break;
+
+            if(cmd.equals("등록")){
+                System.out.print("명언 : ");
+                String quote = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+            }
+        }
     }
 }
