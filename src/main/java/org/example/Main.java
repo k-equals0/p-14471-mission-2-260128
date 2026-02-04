6b2cde98350279f2a4e09f16130bb68e10585b4f
k-equals0 <sallykey@naver.com>
2026-02-04 17:14:09 +0900

    키보드로 입력

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+
+        String cmd = sc.nextLine();
+
+    }
+}
