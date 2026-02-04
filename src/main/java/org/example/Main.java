bf11e6572139fd29424596b1b76636cba26c70af
k-equals0 <sallykey@naver.com>
2026-02-04 22:52:27 +0900

    목록 기능 구현(4) 배열에 저장해서 내림차순 출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,10 @@ public class Main {
     static void main() {
 
         Scanner sc = new Scanner(System.in);
-        int lastId = 0;             //id와 별개
-        Quote quote = new Quote();  //객체 생성
+        int lastId = 0;                 //객체 id
+
+        Quote[] quotes = new Quote[10]; //객체 저장할 배열 생성
+        int lastQuoteIndex = -1;        //객체배열 인덱스
 
         System.out.println("== 명언 앱 ==");
 
@@ -19,21 +21,25 @@ public class Main {
                 break;
 
             if(cmd.equals("등록")){
-                System.out.print("명언 : ");      //키보드로 받기
+                System.out.print("명언 : ");
                 String content = sc.nextLine();
                 System.out.print("작가 : ");
                 String author = sc.nextLine();
 
-                quote.id = ++lastId;        //전위 연산, 객체에 저장
+                Quote quote = new Quote();          //등록 시 객체 생성
+
+                quote.id = ++lastId;
                 quote.content = content;
                 quote.author = author;
 
+                quotes[++lastQuoteIndex] = quote;   //객체배열에 객체 저장
                 System.out.println(lastId + "번 명언이 등록되었습니다.");
             }
             else if(cmd.equals("목록")){
                 System.out.println("번호 / 작가 / 명언\n--------------------------");
-                for(int i=lastId; i>0; i--){    //아직 구현 안됨
-                    System.out.println(quote.id + " / " + quote.author + " / " + quote.content);
+                for(int i=lastQuoteIndex; i>=0; i--){
+                    Quote foundedQuote = quotes[i];     //현재 처리중인 명언임을 드러내기 위함
+                    System.out.println(foundedQuote.id + " / " + foundedQuote.author + " / " + foundedQuote.content);
                 }
             }
         }
